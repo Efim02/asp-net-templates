7206d8ccfe68d280993e851cb1ba9eded8efa508
Efim <efim.zhabin@mail.ru>
2025-02-16 19:04:38 +0700

    SPO-142 Поправил настройки парсинга для StringContent

--- src/Helpful.Api/Json/JsonStringContentExtensions.cs
+++ src/Helpful.Api/Json/JsonStringContentExtensions.cs
@@ -19,6 +19,6 @@ public static class JsonStringContentExtensions
     public static async Task<T> FromJsonStringContent<T>(this HttpContent content)
     {
         var json = await content.ReadAsStringAsync();
-        return JsonConvert.DeserializeObject<T>(json)!;
+        return JsonConvert.DeserializeObject<T>(json, JsonUtils.GetReferencedSettings())!;
     }
 }
