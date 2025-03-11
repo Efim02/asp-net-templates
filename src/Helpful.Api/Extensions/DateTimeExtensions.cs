4f9c4046815f2d2e14813d68fd02692ad2778ef2
Efim <efim.zhabin@mail.ru>
2025-03-11 22:41:37 +0700

    SPO-154 [TODO] В процессе реализации клиента для обучаемых и изображений

---
+++ src/Helpful.Api/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Helpful.Api.Extensions;
+
+public static class DateTimeExtensions
+{
+    /// <summary>
+    /// The number of milliseconds since
+    /// the "Unix epoch" 1970-01-01T00:00:00Z (UTC).
+    /// This value is independent of the time zone.
+    /// </summary>
+    public static long MillisecondsSinceEpoch(this DateTime dateTime)
+    {
+        var universalDateTime = dateTime.ToUniversalTime();
+        return new DateTimeOffset(universalDateTime).ToUnixTimeMilliseconds();
+    }
+    
+    /// <summary>
+    /// The DateTime since the "Unix epoch" 1970-01-01T00:00:00Z (UTC).
+    /// </summary>
+    public static DateTime DateTimeSinceEpoch(this long timeStamp)
+    {
+        return DateTimeOffset.FromUnixTimeMilliseconds(timeStamp).UtcDateTime;
+    }
+}
