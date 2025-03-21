805864ff13a7c04983ed1ba9f1979d1acdcd7a17
Efim <efim.zhabin@mail.ru>
2025-03-22 02:24:18 +0700

    Добавил расширение для Mappings IdentityExtensions.cs

---
+++ src/Helpful.Api/Auth/IdentityExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Helpful.Api.Auth;
+
+using System.Security.Claims;
+
+public static class IdentityExtensions
+{
+    public static ClaimsPrincipal ToClaimsPrincipal(this ClaimsIdentity identity)
+    {
+        return new ClaimsPrincipal(identity);
+    }
+}
