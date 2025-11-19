2706533d4af70a274d17e8de56634f7fc47a2521
Efim <49785820+Efim02@users.noreply.github.com>
2025-11-19 15:23:13 +0700

    Add files via upload

---
+++ src/Helpful.Core/Extensions/IntExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace SYNC.BL.Extensions;
+
+using System;
+
+/// <summary>
+/// Утилита для типа Int.
+/// </summary>
+public static class IntUtility
+{
+    /// <summary>
+    /// Сжатая реализация округления до целого числа.
+    /// </summary>
+    /// <param name="value"> Значение. </param>
+    /// <returns> Результат округления. </returns>
+    public static int Round(this double value)
+    {
+        return (int) Math.Round(value);
+    }
+
+    /// <summary>
+    /// Берет число в промежутке.
+    /// </summary>
+    public static int Clamp(this int value, int min = 0, int max = 1)
+    {
+        return value < min ? min : value > max ? max : value;
+    }
+}
