e21ffc0188f19cb27fe86e443b561dceeb22cadb
Efim <49785820+Efim02@users.noreply.github.com>
2025-11-26 11:50:21 +0700

    Update LinqExtensions.cs

--- src/Helpful.Core/Extensions/Linq/LinqExtensions.cs
+++ src/Helpful.Core/Extensions/Linq/LinqExtensions.cs
@@ -78,4 +78,125 @@ public static class LinqExtensions
     /// Объединяет строки через разделитель.
     /// </summary>
     public static string Join(this IEnumerable<string> source, string separator) => string.Join(separator, source);
+
+    
+        /// <summary>
+        /// Выполняет пересечение меньшего списка из большего списка.
+        /// </summary>
+        /// <param name="enumerable"> Список. </param>
+        /// <param name="anotherEnumerable"> Другой список. </param>
+        /// <param name="getPropertyFunc"> Функция для сравнения. </param>
+        public static IEnumerable<T> IntersectByBigList<T, TP>(
+            this IEnumerable<T> enumerable,
+            IEnumerable<T> anotherEnumerable,
+            Func<T, TP> getPropertyFunc)
+        {
+            var list = enumerable.ToList();
+            var anotherList = anotherEnumerable.ToList();
+
+            var bigList = list.Count > anotherList.Count ? list : anotherList;
+            var smallList = list.Count > anotherList.Count ? anotherList : list;
+
+            return bigList.Intersect(smallList, new ByPropertyComparer<T, TP>(getPropertyFunc));
+        }
+
+        /// <summary>
+        /// Выполняет исключение меньшего списка из большего списка.
+        /// </summary>
+        /// <param name="enumerable"> Список. </param>
+        /// <param name="anotherEnumerable"> Другой список. </param>
+        /// <param name="getPropertyFunc"> Функция для сравнения. </param>
+        public static IEnumerable<T> ExceptByBigList<T, TP>(
+            this IEnumerable<T> enumerable,
+            IEnumerable<T> anotherEnumerable,
+            Func<T, TP> getPropertyFunc)
+        {
+            var list = enumerable.ToList();
+            var anotherList = anotherEnumerable.ToList();
+
+            var bigList = list.Count > anotherList.Count ? list : anotherList;
+            var smallList = list.Count > anotherList.Count ? anotherList : list;
+
+            return bigList.Except(smallList, new ByPropertyComparer<T, TP>(getPropertyFunc));
+        }
+
+        /// <summary>
+        /// Получает список, который должен иметь последовательность элементов как в указанной последовательности.
+        /// В случае если не сработает, ошибки не возвращает.
+        /// </summary>
+        public static List<T> GetListLikeSequence<T>(
+            this IEnumerable<T> collectionEnumerable,
+            IEnumerable<T> sequenceEnumerable,
+            Func<T, IComparable> getPropertyFunc = null)
+            where T : class
+        {
+            var collection = collectionEnumerable.ToList();
+            var sequence = sequenceEnumerable.ToList();
+
+            // Если нет ни одного равного элемента, возвращаем тот же список.
+            var firstItem = sequence.FirstOrDefault(i => collection
+                .Any(ai => EqualsByFuncOrDefault(getPropertyFunc, i, ai)));
+            if (firstItem == null)
+                return collection.ToList();
+
+            // Если нет второго равного элемента, возвращаем список с этого элемента.
+            var secondItem = sequence.FirstOrDefault(item => collection
+                .Any(anotherItem => anotherItem != firstItem
+                                    && EqualsByFuncOrDefault(getPropertyFunc, item, anotherItem)));
+            if (secondItem == null)
+                return collection.StartWith(firstItem, getPropertyFunc).ToList();
+
+            var list = collection.StartWith(firstItem, getPropertyFunc).ToList();
+
+            // Список с обратной последовательностью, и начинающийся с того же первого элемента; для проверки ниже.
+            var reversedList = list.ToList();
+            reversedList.Reverse();
+            reversedList = reversedList.StartWith(firstItem, getPropertyFunc).ToList();
+
+            // Для случая равно, может отработать криво, как отработает:
+            // возможно нужно будет переделать на условие через 1-2 элемент (был такой вариант проверять).
+            var resultList = CountSequentiallyEquals(list, sequence) >= CountSequentiallyEquals(reversedList, sequence)
+                ? list
+                : reversedList;
+
+            if (collection.Count != resultList.Count)
+                throw new Exception("Ошибка метода GetListLikeSequence, ошибочное кол-во элементов на выходе");
+
+            return resultList;
+        }
+
+        /// <summary>
+        /// Получает количество последовательно расположенных равных элементов.
+        /// </summary>
+        public static int CountSequentiallyEquals<T>(
+            this IEnumerable<T> elements,
+            List<T> anotherElements,
+            Func<T, IComparable> getPropertyFunc = null)
+            where T : class
+        {
+            var countEquals = 0;
+            var previousAnotherElementIndex = 0;
+
+            foreach (var element in elements)
+            {
+                // Если нет такого элемента или индекс равен - пропускаем.
+                var anotherElement = anotherElements
+                    .FirstOrDefault(ai => EqualsByFuncOrDefault(getPropertyFunc, element, ai));
+
+                // Проверка на null, IndexOf вдруг исключение выкинет.
+                var anotherElementIndex = anotherElement == null ? -1 : anotherElements.IndexOf(anotherElement);
+                if (anotherElementIndex == -1 || anotherElementIndex == previousAnotherElementIndex)
+                    continue;
+
+                // Если предыдущий индекс больше, значит уже не последовательно расположены.
+                if (previousAnotherElementIndex > anotherElementIndex)
+                    break;
+
+                previousAnotherElementIndex = anotherElementIndex;
+                countEquals++;
+            }
+
+            return countEquals;
+        }
+
 }
