b39d15ef5eb6102533803ba9103415454bbe5511
Efim <49785820+Efim02@users.noreply.github.com>
2025-11-27 15:23:11 +0700

    Create StackExtensions.cs

---
+++ src/Helpful.Core/Extensions/Linq/StackExtensions.cs
@@ -0,0 +1,20 @@
+    /// <summary>
+    /// Расширение для Stack.
+    /// </summary>
+    public static class StackExtensions
+    {
+        /// <summary>
+        /// Достает элемент или если нет элементов возвращает null.
+        /// </summary>
+        public static T PopOrDefault<T>(this Stack<T> stack)
+        {
+            return stack.Count == 0 ? default : stack.Pop();
+        }
+        /// <summary>
+        /// Просматривает элемент или если нет элементов возвращает null.
+        /// </summary>
+        public static T PeekOrDefault<T>(this Stack<T> stack)
+        {
+            return stack.Count == 0 ? default : stack.Peek();
+        }
+    }
